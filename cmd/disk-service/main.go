eb6ed997a852551180858cea88c05286ea955c78
Sheikh Gadzhialiev <sheixpeer@gmail.com>
2026-04-14 03:35:44 +0300

    Add slog logger

--- cmd/disk-service/main.go
+++ cmd/disk-service/main.go
@@ -1,21 +1,49 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 
 	"github.com/sheixpeer/disk-service/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
 
-	fmt.Println(cfg)
-	// TODO: init logger: slog
+	log := setupLogger(cfg.Env)
 
-	// TODO: init storage/repository: postgres
+	log.Info("starting disk-service", slog.String("env", cfg.Env))
+	log.Debug("debug message are enabled")
 
 	// TODO: init router: chi, "chi render"
 
 	// TODO: run server
 
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
